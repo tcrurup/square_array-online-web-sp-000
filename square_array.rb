394a6adf52d602f2e01a6ef14e60346f3dc872a6
Anthony Rurup <tcrurup@gmail.com>
2019-04-25 21:15:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,12 @@
 require 'pry'
 
-##
-#def square_array(array)
-#  squared_numbers = []
-#  array.each do |number|
-#    squared_numbers << number * number
-#  end
-#  squared_numbers
-#end
-
 def square_array(array)
-  squared_numbers = array.collect{ |x| x*x }
+  squared_numbers = []
+  array.each do |number|
+    squared_numbers << number * number
+  end
   squared_numbers
 end
+
+
 
